df1d8eafe8e5948122a24650278126cc0a63378e
TechTastic <74630543+TechTastic@users.noreply.github.com>
2024-06-09 21:17:45 -0500

    Removed unused imports

--- src/main/kotlin/io/github/techtastic/scriptables/item/ScriptItem.kt
+++ src/main/kotlin/io/github/techtastic/scriptables/item/ScriptItem.kt
@@ -1,33 +1,17 @@
 package io.github.techtastic.scriptables.item
 
-import io.github.techtastic.scriptables.Scriptables.LOGGER
 import io.github.techtastic.scriptables.api.ScriptablesAPI
 import io.github.techtastic.scriptables.api.lua.Script
 import io.github.techtastic.scriptables.screen.ScriptEditorScreen
-import kotlinx.coroutines.handleCoroutineException
 import net.minecraft.client.Minecraft
-import net.minecraft.nbt.CompoundTag
-import net.minecraft.network.chat.Component
 import net.minecraft.world.InteractionHand
-import net.minecraft.world.InteractionResult
 import net.minecraft.world.InteractionResultHolder
-import net.minecraft.world.MenuProvider
-import net.minecraft.world.entity.Entity
-import net.minecraft.world.entity.player.Inventory
 import net.minecraft.world.entity.player.Player
-import net.minecraft.world.inventory.AbstractContainerMenu
 import net.minecraft.world.item.Item
 import net.minecraft.world.item.ItemStack
-import net.minecraft.world.item.context.UseOnContext
-import net.minecraft.world.level.ClipContext
 import net.minecraft.world.level.Level
-import net.minecraft.world.phys.shapes.CollisionContext
 
 class ScriptItem(properties: Properties) : Item(properties) {
-    val script = object: Script() {
-        override fun reload() { }
-    }
-
     override fun use(
         level: Level,
         player: Player,
