aa038386a42f5fc09499ed57001f49d25d918000
techtastic <noreply@gmail.com>
2024-06-11 16:30:27 -0500

    Back to Screens Added new textures for EditorScreen

--- src/main/kotlin/io/github/techtastic/scriptables/screen/ScriptEditorScreen.kt
+++ src/main/kotlin/io/github/techtastic/scriptables/screen/ScriptEditorScreen.kt
@@ -2,9 +2,11 @@ package io.github.techtastic.scriptables.screen
 
 import com.mojang.blaze3d.systems.RenderSystem
 import io.github.techtastic.scriptables.Scriptables
+import io.github.techtastic.scriptables.Scriptables.LOGGER
+import io.github.techtastic.scriptables.Scriptables.getWithModId
 import io.github.techtastic.scriptables.api.lua.Script
 import net.minecraft.client.gui.GuiGraphics
-import net.minecraft.client.gui.components.CodeEditBox
+import net.minecraft.client.gui.components.*
 import net.minecraft.client.gui.screens.Screen
 import net.minecraft.client.renderer.GameRenderer
 import net.minecraft.network.chat.Component
@@ -12,8 +14,10 @@ import net.minecraft.network.chat.Component
 class ScriptEditorScreen(val script: Script): Screen(Component.translatable("gui.scriptables.script_editor")) {
     private val TEXTURE = Scriptables.getWithModId("textures/gui/script_editor.png")
     private lateinit var codeEditor: CodeEditBox
+    private lateinit var errorShown: MultilineTextField
+    private lateinit var console: EditBox
 
-    val imageWidth = 176
+    val imageWidth = 199
     val imageHeight = 222
 
     override fun init() {
@@ -23,17 +27,29 @@ class ScriptEditorScreen(val script: Script): Screen(Component.translatable("gui
 
         this.codeEditor = CodeEditBox(
             this.font,
-            x + 43,
+            x + 7,
             y + 17,
-            126,
-            142,
+            162,
+            198,
             Component.literal("while true do\n    --your code here\nend"),
             Component.literal("Code Editor"),
             this.script
         )
         this.addRenderableWidget(this.codeEditor)
 
-        //TOOD: Add buttons, errors, and console
+        //TODO: Upload Button: 177, 31
+        this.addRenderableWidget(
+            ImageButton(x + 177, y + 31, 16, 16, WidgetSprites(getWithModId("buttoms/upload_button"), getWithModId("buttoms/upload_button_hovered"))
+            ) { button -> LOGGER.info("File trying ot be uploaded!") }
+        )
+        //TODO: Save Button: 177, 63
+        this.addRenderableWidget(
+            ImageButton(x + 177, y + 63, 16, 16, WidgetSprites(getWithModId("buttoms/save_button"), getWithModId("buttoms/save_button_hovered"))
+            ) { button -> LOGGER.info("File trying ot be uploaded!") }
+        )
+        //TODO: File Button: 177, 79
+        //TODO: Error Multiline: 7, 139 by 162 x 65
+        //TODO: Console Input: 7, 204 by 162 x 11
     }
 
     override fun renderBackground(guiGraphics: GuiGraphics, i: Int, j: Int, f: Float) {
